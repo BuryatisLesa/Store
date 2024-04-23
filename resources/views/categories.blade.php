404de28ff1938224835f49127d9215ca7c86e20e
Konstantin <kostyasandanov@gmail.com>
2024-04-23 15:28:48 +0800

    Добавлены html странички, добавлены новые маршрутизаторы

---
+++ resources/views/categories.blade.php
@@ -0,0 +1,50 @@
+
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>MyStore</title>
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="/css/styles.css" rel="stylesheet" />
+</head>
+<body>
+<!-- Responsive navbar-->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a class="navbar-brand" href="/">MyStore</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item"><a class="nav-link active" aria-current="page" href="/categories">Категории</a></li>
+                <li class="nav-item"><a class="nav-link" href="/contact/">Все товары</a></li>
+                <a class="nav-link dropdown-toggle" id="navbarDropdown" href="/admin" role="button" data-bs-toggle="dropdown" aria-expanded="false">Войти</a>
+                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li><hr class="dropdown-divider" /></li>
+                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<!-- Page content-->
+<div class="container">
+    <div class="text-center mt-5">
+    </div>
+    <div >
+        <hr>
+        Категории:<br>
+        <a href="/mobiles">Мобильная техника</a><br>
+        <a href="/portables">Портативная техника</a><br>
+        <a href="/computers">Компьютерная техника</a><br>
+        <hr>
+    </div>
+</div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'App\Http\Controllers\MainController@index');
+Route::get('/categories', 'App\Http\Controllers\MainController@categories');
+Route::get('/{categories}', 'App\Http\Controllers\MainController@category');
+Route::get('/product/{product?}', 'App\Http\Controllers\MainController@product');
+
